f06d561169a45d93cc60afdb49325031645bb0a0
clemence95 <boitelleclemence@outlook.fr>
2024-08-20 14:23:07 +0200

    .

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import java.sql.Connection;
+import java.sql.SQLException;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            if (connection != null) {
+                System.out.println("Connexion à la base de données réussie !");
+            } else {
+                System.out.println("Échec de la connexion à la base de données.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
